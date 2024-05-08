682a5880ee9b27d127d52733b91d942d9d9e912c
cass <cassianobruno1997@hotmail.com>
2024-05-08 17:56:51 -0300

    Implementing get nearst places

--- src/services/maps.service.ts
+++ src/services/maps.service.ts
@@ -1,13 +1,40 @@
 import { Place } from '../models/place.model'
 import axios from 'axios'
 
-enum PlaceType {
-    recordStudio = 'RecordStudio',
-    podcastStudio = 'PodcastStudio',
-    audioVisualStudio = 'AudioVisualStudio' 
+type Filter = {
+    openNow?: boolean;
+    type: PlaceType;
 }
 
-const getNearestPlaces = (latitude: string, longitude: string, radius: number, placeType: PlaceType): Promise<Place[]> => 
-    Promise.resolve(new Array<Place>());
+enum PlaceType {
+    recordStudio = 'Record Studios',
+    podcastStudio = 'Podcast Studios',
+    audioVisualStudio = 'Audiovisual Studios',
+}
+
+const getNearestPlaces = async (latitude: string, longitude: string, radius: number, placeType: PlaceType): Promise<Place[]> => {
+    const body = {
+      textQuery: placeType.toString(),
+      openNow: false,
+      locationBias: {
+        circle: {
+          center: { latitude: latitude, longitude: longitude },
+          radius: radius,
+        },
+      },
+    };
+
+    const apiResource = 'places:searchText'
+;
+    let response = await axios.post(
+      `https://places.googleapis.com/v1/${apiResource}`,
+      body,
+      { headers: { "X-Goog-FieldMask": "*",
+      "X-Goog-Api-Key": "" }}
+    );
+
+    return response.data;
+
+}
 
 export { PlaceType, getNearestPlaces }
